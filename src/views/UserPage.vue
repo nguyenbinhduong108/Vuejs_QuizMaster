61a4dbec193cbaf3621b2929952f66177caa09b6
Duong Nguyen <binhduong10082002@gmail.com>
2024-01-31 14:20:42 +0700

    update: update scroll

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -8,7 +8,7 @@
       <div class="!pt-[50px] min-h-[calc(100vh-50px)] w-full xl:w-4/5 2xl:w-3/4 my-6 px-4 flex justify-center">
         <div class="flex flex-col lg:flex-row gap-8 overflow-hidden pt-4">
           <!-- menu -->
-          <div v-if="categories.length > 0" class="flex lg:!flex-col gap-2">
+          <div v-if="categories.length > 0" class="flex overflow-x-scroll lg:!flex-col gap-2 custom-scrollbar">
             <div @click="() => {
               getAllQuestion();
               selectCategory(null);
